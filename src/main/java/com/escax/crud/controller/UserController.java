d1a45138e71301e3e01567982ff96f9c3ddd03c7
Escax <cardozoesneider139@gmail.com>
2025-03-06 10:10:17 -0500

    Se agrega Swagger, se realiza el service y los controllers de users y orders

---
+++ src/main/java/com/escax/crud/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.escax.crud.controller;
+
+import com.escax.crud.dto.UserDTO;
+import com.escax.crud.service.UserService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping
+    public ResponseEntity<UserDTO> createUser(@Valid @RequestBody UserDTO userDTO) {
+        return ResponseEntity.ok(userService.createUser(userDTO));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<UserDTO> getUserById(@PathVariable Long id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UserDTO>> searchUsersForName(@RequestParam(required = false) String name) {
+        return ResponseEntity.ok(userService.searchUsersForName(name));
+    }
+}
---
+++ src/main/java/com/escax/crud/service/UserService.java
@@ -0,0 +1,42 @@
+package com.escax.crud.service;
+
+import com.escax.crud.dto.UserDTO;
+import com.escax.crud.entity.User;
+import com.escax.crud.mapper.UserMapper;
+import com.escax.crud.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+
+    @Autowired
+    public UserService(UserRepository userRepository, UserMapper userMapper) {
+        this.userRepository = userRepository;
+        this.userMapper = userMapper;
+    }
+
+    public UserDTO createUser(UserDTO userDTO) {
+        User user = userMapper.toEntity(userDTO);
+        user = userRepository.save(user);
+        return userMapper.toDTO(user);
+    }
+
+    public UserDTO getUserById(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        return userMapper.toDTO(user);
+    }
+
+    public List<UserDTO> searchUsersForName(String name) {
+        List<User> users = userRepository.findByNameContainingIgnoreCase(name);
+        return users.stream().map(userMapper::toDTO).toList();
+    }
+
+
+}
